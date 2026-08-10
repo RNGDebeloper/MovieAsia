995370ed47594ddbf14ea7c5fb2cbe6b050adbb4
RNGDebeloper <131392755+RNGDebeloper@users.noreply.github.com>
2026-08-10 20:31:00 +0530

    Update page.tsx
    g

--- src/app/watch/tv/[slug]/page.tsx
+++ src/app/watch/tv/[slug]/page.tsx
@@ -3,7 +3,25 @@ import EmbedPlayer from '@/components/watch/embed-player';
 
 export const revalidate = 3600;
 
-export default function Page({ params }: { params: { slug: string } }) {
-  const id = params.slug.split('-').pop();
-  return <EmbedPlayer url={`https://vidsrc.sbs/embed/tv/${id}`} />;
+export default function Page({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  // Example slug:
+  // 79744-1-1
+  //
+  // 79744 = TMDB ID
+  // 1     = Season
+  // 1     = Episode
+
+  const parts = params.slug.split('-');
+
+  const id = parts[0];
+  const season = parts[1] || '1';
+  const episode = parts[2] || '1';
+
+  const embedUrl = `https://vidsrc.sbs/embed/tv/${id}/${season}/${episode}`;
+
+  return <EmbedPlayer url={embedUrl} />;
 }
